bde53695717cb60f85bda333b5da5a77c0eb7e33
Andreas Beham <andreas.beham@gmx.at>
2024-08-25 22:35:51 +0200

    Add unit tests
    - Add Catch2 dependency

---
+++ src/Tests/BasicTests.cpp
@@ -0,0 +1,89 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "Countdown.hpp"
+
+TEST_CASE( "Basic usage of Countdown", "[Countdown]" )
+{
+    const int timeoutms = 100;
+    bool called = false;
+    Countdown cd{std::chrono::milliseconds{timeoutms}, [&called] () {
+        called = true;
+    }};
+    SECTION("Wait for callback")
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(timeoutms * 2));
+        REQUIRE(called);
+    }
+    SECTION("Abort")
+    {
+        cd.Abort();
+        std::this_thread::sleep_for(std::chrono::milliseconds(timeoutms * 2));
+        REQUIRE(!called);
+    }
+}
+
+TEST_CASE( "Countdown with different durations", "[Countdown]" )
+{
+    const int timeoutms = 100;
+    bool called = false;
+    Countdown cd{std::chrono::milliseconds{timeoutms}, [&called] () {
+        called = true;
+    }};
+    std::this_thread::sleep_for(std::chrono::milliseconds(timeoutms / 2));
+    REQUIRE(!called);
+    std::this_thread::sleep_for(std::chrono::milliseconds(timeoutms));
+    REQUIRE(called);
+}
+
+TEST_CASE( "Multiple Countdowns", "[Countdown]" )
+{
+    const int timeoutms = 100;
+    bool called1 = false, called2 = false;
+    Countdown cd1{std::chrono::milliseconds{timeoutms}, [&called1] () {
+        called1 = true;
+    }};
+    Countdown cd2{std::chrono::milliseconds{timeoutms}, [&called2] () {
+        called2 = true;
+    }};
+    std::this_thread::sleep_for(std::chrono::milliseconds(timeoutms * 2));
+    REQUIRE(called1);
+    REQUIRE(called2);
+}
+
+TEST_CASE( "Aborting multiple times has no effect", "[Countdown]" )
+{
+    const int timeoutms = 100;
+    bool called = false;
+    Countdown cd{std::chrono::milliseconds{timeoutms}, [&called] () {
+        called = true;
+    }};
+    cd.Abort();
+    cd.Abort();
+    std::this_thread::sleep_for(std::chrono::milliseconds(timeoutms * 2));
+    REQUIRE(!called);
+}
+
+TEST_CASE( "Aborting after callback has no effect", "[Countdown]" )
+{
+    const int timeoutms = 100;
+    bool called = false;
+    Countdown cd{std::chrono::milliseconds{timeoutms}, [&called] () {
+        called = true;
+    }};
+    std::this_thread::sleep_for(std::chrono::milliseconds(timeoutms * 2));
+    cd.Abort();
+    REQUIRE(called);
+}
+
+TEST_CASE( "Going out of scope also aborts the countdown", "[Countdown]" )
+{
+    const int timeoutms = 100;
+    bool called = false;
+    {
+        Countdown cd{std::chrono::milliseconds{timeoutms}, [&called] () {
+            called = true;
+        }};
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(timeoutms * 2));
+    REQUIRE(!called);
+}
